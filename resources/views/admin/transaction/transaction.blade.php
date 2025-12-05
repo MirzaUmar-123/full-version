6a67548da95d1d642deaf6c6d1aa41312296ae6d
Mirza Umar Islam <mu5644370@gmail.com>
2025-12-05 12:15:20 +0500

    Updated transaction

--- resources/views/admin/transaction/transaction.blade.php
+++ resources/views/admin/transaction/transaction.blade.php
@@ -11,7 +11,7 @@
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="#"><i class="las la-home"></i>Home</a></li>
-                                <li class="breadcrumb-item active" aria-current="page">Transactions</li>
+                                <li class="breadcrumb-item active" aria-current="page">Transactions Management</li>
                             </ol>
                         </nav>
                     </div>
