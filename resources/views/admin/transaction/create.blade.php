47c03d4f1fc8a493eba6a537b141326f785a7ad9
Mirza Umar Islam <mu5644370@gmail.com>
2025-12-06 11:36:35 +0500

    transaction create file updated

--- resources/views/admin/transaction/create.blade.php
+++ resources/views/admin/transaction/create.blade.php
@@ -2,4 +2,83 @@
 @section('title',$title)
 @section('description',$description)
 @section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-main">
+                    <h4 class="text-capitalize breadcrumb-title">Create New Transaction</h4>
+                    <div class="breadcrumb-action justify-content-center flex-wrap">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#"><i class="las la-home"></i>Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Create Transaction</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Add New Transaction</h5>
+                </div>
+                <form method="POST" action="#">
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="userID" class="form-label">User ID</label>
+                            <input type="number" class="form-control" id="userID" placeholder="Enter user ID"
+                                required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="orderID" class="form-label">Order ID</label>
+                            <input type="number" class="form-control" id="orderID" placeholder="Enter order ID"
+                                required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="paymentID" class="form-label">Payment ID</label>
+                            <input type="number" class="form-control" id="paymentID" placeholder="Enter Payment ID"
+                                required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="amount" class="form-label">Amount</label>
+                            <input type="number" class="form-control" id="amount" placeholder="Enter Amount" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="type" class="form-label">Type</label>
+                            <select class="form-control" id="type" required>
+                                <option value="selecttype">Select Type</option>
+                                <option value="charge">Charge</option>
+                                <option value="refund">Refund</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="gateway" class="form-label">Gateway</label>
+                            <select class="form-control" id="gateway" required>
+                                <option value="selectgateway">Select Gateway</option>
+                                <option value="stripe">Stripe</option>
+                                <option value="paypal">Paypal</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="reference" class="form-label">Reference</label>
+                            <input type="text" class="form-control" id="reference" placeholder="Enter Reference " required>
+                        </div>
+                        <div class="mb-3">
+                                <label for="status">Status</label>
+                                <select class="form-control" id="status" name="status" required>
+                                    <option value="pending">Select Status</option>
+                                    <option value="success">Success</option>
+                                    <option value="pending">Pending</option>
+                                    <option value="failed">Failed</option>
+                                    <option value="cancelled">Cancelled</option>
+                                </select>
+                            </div>
+                    </div>
+                    <div class="modal-footer justify-content-start">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+            </div>
 @endsection
