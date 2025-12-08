f9b8f0d10ab0841c6d7e080db4e4e29016ee7fcc
Mirza Umar Islam <mu5644370@gmail.com>
2025-12-08 10:51:07 +0500

    Categories.index file Updated

--- resources/views/admin/categories/categories.blade.php
+++ resources/views/admin/categories/categories.blade.php
@@ -93,6 +93,11 @@
                                             <td>
                                                 <ul class="d-flex  gap-2">
                                                     <li>
+                                                        <a href="#" class="btn btn-primary btn-sm" title="View">
+                                                            <i class="uil uil-eye"></i>
+                                                        </a>
+                                                    </li>
+                                                    <li>
                                                         <a href="#" class="btn btn-primary btn-sm" title="Edit">
                                                             <i class="uil uil-edit"></i>
                                                         </a>
