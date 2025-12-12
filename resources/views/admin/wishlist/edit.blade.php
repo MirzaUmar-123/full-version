66654d450fe17fe9b4a3df97c6b5e44636de8189
Mirza Umar Islam <mu5644370@gmail.com>
2025-12-12 11:43:37 +0500

    Updated Wishlist.Edit File

--- resources/views/admin/wishlist/edit.blade.php
+++ resources/views/admin/wishlist/edit.blade.php
@@ -2,4 +2,46 @@
 @section('title',$title)
 @section('description',$description)
 @section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="breadcrumb-main">
+                    <h4 class="text-capitalize breadcrumb-title">Edit Wishlist</h4>
+                    <div class="breadcrumb-action justify-content-center flex-wrap">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#"><i class="las la-home"></i>Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Edit Wishlist</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mb-30">
+                <div class="card">
+                    <div class="card-header">
+                        <h6 class="mb-0">Edit Wishlist</h6>
+                    </div>
+                    <div class="card-body pt-0">
+                        <form>
+                            <div class="form-group">
+                                <label for="userid">User ID</label>
+                                <input type="number" class="form-control" id="userid" value="{{old('user_id',$wishlist->user_id)}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="productid">Product ID</label>
+                                <input type="number" class="form-control" id="productid" value="{{old('product_id',$wishlist->product_id)}}">
+                            </div>
+                            <div class="modal-footer justify-content-start ml-0">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
